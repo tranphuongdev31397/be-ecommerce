d31ad7c72fd2c052c116d17afe54176a2928ea55
Eddie Tran <tranphuongdev31397@gmail.com>
2023-12-19 19:59:54 +0700

    update: func removeUndefinedNullNestedObject

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,8 +14,44 @@ const getUnselectData = (selectArr = []) => {
   return Object.fromEntries(_.map(selectArr, key => [key, 0]))
 }
 
+// const removeNullUndefinedNestedObject = obj => {
+//   if (typeof obj !== 'object') {
+//     return obj
+//   }
+
+//   const result = _.isArray(obj) ? [] : {}
+
+//   _.forEach(obj, (value, key) => {
+//     if (typeof value === 'object' && value !== null) {
+//       const newValue = removeNullUndefinedNestedObject(value)
+//       result[key] = newValue
+//     } else if (!_.isUndefined(value) && !_.isNull(value)) {
+//       result[key] = value
+//     }
+//   })
+
+//   return result
+// }
+
+function removeUndefinedAndNullNestedObject(obj) {
+  return _.transform(obj, (result, value, key) => {
+    if (_.isObject(value)) {
+      // Recursively clean nested objects
+      const cleanedValue = removeUndefinedAndNull(value)
+      // Omit key if value is an empty object or array
+      if (!(_.isEmpty(cleanedValue) && _.isArray(value))) {
+        result[key] = cleanedValue
+      }
+    } else if (!_.isUndefined(value) && !_.isNull(value)) {
+      // Include key-value pairs if value is not undefined or null
+      result[key] = value
+    }
+  })
+}
+
 module.exports = {
   getInitData,
   getSelectData,
   getUnselectData,
+  removeUndefinedAndNullNestedObject,
 }
