f62a6f98b6ba138e674dddeb028cb7c6abd5382b
Eddie Tran <tranphuongdev31397@gmail.com>
2023-12-24 14:49:00 +0700

    update: func checkDiscountCodeIsExist

--- src/models/repositories/discount.repo.js
+++ src/models/repositories/discount.repo.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const { convertToMongoObjectId } = require('../../utils')
+const { BadRequestError } = require('../../core/error.response')
+const { convertToMongoObjectId, getSelectData } = require('../../utils')
 const discountModel = require('../discount.model')
 
 const updateDiscountByShop = async ({
@@ -24,20 +25,46 @@ const updateDiscountByShop = async ({
   )
 }
 
+const findAllDiscount = async ({ limit, page, sort, filter, select }) => {
+  const skip = (page - 1) * limit
+  const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 }
+  return discountModel
+    .find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit)
+    .select(getSelectData(select))
+    .lean()
+}
+
 const checkDiscountCodeIsExist = async ({
   discount_code,
   _id,
+  shopId,
+  isDiscountGlobal = false,
   isUpdate = true,
 }) => {
   // Func check when update an discount
 
   const filter = {
     discount_code,
+    discount_shop: convertToMongoObjectId(shopId),
     _id: { $ne: convertToMongoObjectId(_id) },
   }
 
+  if (isUpdate && !_id) {
+    return null
+  }
+
+  if (!isDiscountGlobal && !discount_shop) {
+    return null
+  }
   if (!isUpdate) {
     delete filter._id
+  }
+
+  if (isDiscountGlobal) {
+    delete filter.discount_shop
   }
 
   return await discountModel.findOne(filter).lean()
@@ -46,4 +73,5 @@ const checkDiscountCodeIsExist = async ({
 module.exports = {
   updateDiscountByShop,
   checkDiscountCodeIsExist,
+  findAllDiscount,
 }
