6de8f061374461b5ffe19e4ed0d85ac94080be01
Eddie Tran <tranphuongdev31397@gmail.com>
2023-12-25 17:09:19 +0700

    update: remove code unsed

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,25 +23,6 @@ const convertToMongoObjectId = id => {
   return new Types.ObjectId(id)
 }
 
-// const removeNullUndefinedNestedObject = obj => {
-//   if (typeof obj !== 'object') {
-//     return obj
-//   }
-
-//   const result = _.isArray(obj) ? [] : {}
-
-//   _.forEach(obj, (value, key) => {
-//     if (typeof value === 'object' && value !== null) {
-//       const newValue = removeNullUndefinedNestedObject(value)
-//       result[key] = newValue
-//     } else if (!_.isUndefined(value) && !_.isNull(value)) {
-//       result[key] = value
-//     }
-//   })
-
-//   return result
-// }
-
 function removeUndefinedAndNullNestedObject(obj) {
   return _.transform(obj, (result, value, key) => {
     if (_.isObject(value)) {
