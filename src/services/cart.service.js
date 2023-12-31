d910d6974f81489a9d068b035a678ca8322778d0
Eddie Tran <tranphuongdev31397@gmail.com>
2023-12-31 11:01:15 +0700

    update: addToCart API: remove product with quantity eq 0

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { find } = require('lodash')
+const { some } = require('lodash')
 const { BadRequestError, NotFoundError } = require('../core/error.response')
 const cartModel = require('../models/cart.model')
 const { getDetailProduct } = require('../models/repositories/product.repo')
@@ -30,13 +30,16 @@ class CartService {
         'cart_products.id': id,
         cart_userId: convertToMongoObjectId(userId),
       },
+
       {
-        $inc: {
+        $set: {
           'cart_products.$.quantity': quantity,
         },
       },
+
       {
         new: true,
+        upsert: true,
       },
     )
 
@@ -72,16 +75,34 @@ class CartService {
       throw new BadRequestError("Can't buy your shop's product")
     }
 
-    if (_cart.cart_products.length === 0) {
+    if (product.quantity <= 0) {
       return await _cart.updateOne(
         {
-          cart_products: [product],
+          $pull: {
+            cart_products: {
+              id: product.id,
+            },
+          },
+        },
+        {
+          new: true,
+        },
+      )
+    }
+
+    if (!some(_cart.cart_products, it => product.id === it.id)) {
+      return await _cart.updateOne(
+        {
+          $push: {
+            cart_products: product,
+          },
         },
         {
           new: true,
         },
       )
     } else {
+      // Quantity > 0 && product available
       return await CartService.updateQuantityProduct({
         product,
         userId,
